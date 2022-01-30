48d2544c8e9ebd7374cc237c26506c1dbca7ecf7
Benoît Hubert <benoithubert@gmail.com>
2022-01-30 23:59:11 +0100

    Added repetition of article tag from an array of posts

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Post {
+  id: number;
+  title: string;
+  body: string
+}
+
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
@@ -9,6 +15,18 @@ export class BlogComponent implements OnInit {
   title = 'My personal blog';
   imageUrl = 'https://angular.io/assets/images/logos/angular/angular.svg';
   isLoading = true;
+  posts: Post[] = [
+    {
+      id: 1,
+      title: 'sunt aut facere repellat',
+      body: 'quia et suscipit\nsuscipit recusandae',
+    },
+    {
+      id: 2,
+      title: 'qui est esse',
+      body: 'est rerum tempore vitae\nsequi sint nihil',
+    },
+  ];
 
   constructor() { }
 
